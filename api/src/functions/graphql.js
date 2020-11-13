1f446f67900529bd0d7eae763ce0875eb351e85f
Brandon Noad <Brandon.Noad@gmail.com>
2020-11-12 21:22:13 -0400

    Add Authn

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,20 +1,19 @@
-import {
-  createGraphQLHandler,
-  makeMergedSchema,
-  makeServices,
-} from '@redwoodjs/api'
+import { createGraphQLHandler, makeMergedSchema, makeServices } from '@redwoodjs/api';
 
-import schemas from 'src/graphql/**/*.{js,ts}'
-import services from 'src/services/**/*.{js,ts}'
-import { db } from 'src/lib/db'
+import schemas from 'src/graphql/**/*.{js,ts}';
+import services from 'src/services/**/*.{js,ts}';
+
+import { getCurrentUser } from 'src/lib/auth';
+import { db } from 'src/lib/db';
 
 export const handler = createGraphQLHandler({
-  schema: makeMergedSchema({
-    schemas,
-    services: makeServices({ services }),
-  }),
-  onException: () => {
-    // Disconnect from your database with an unhandled exception.
-    db.$disconnect()
-  },
-})
+    getCurrentUser,
+    schema: makeMergedSchema({
+        schemas,
+        services: makeServices({ services })
+    }),
+    onException: () => {
+        // Disconnect from your database with an unhandled exception.
+        db.$disconnect();
+    }
+});
